1faf4748414473845a84f4dc13fa6da509954d2b
SaboorTunio <saboortunio2021@gmail.com>
2025-02-27 16:33:02 +0500

    Arithmetic Operation in Python

---
+++ Step 05 Arithmetic Operation in Python/main.py
@@ -0,0 +1,21 @@
+# Define two numbers
+a = 10
+b = 3
+
+# Perform arithmetic operations
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+floor_division = a // b
+modulus = a % b
+exponentiation = a ** b
+
+# Print results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor Division:", floor_division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
---
+++ Step 05 Arithmetic Operation in Python/readme.md
@@ -0,0 +1,12 @@
+# Arithmetic Operation in Python
+
+## Basic  Arithmetic Operation:
+### Addition (+)
+### Subtraction (-)
+### Multiplication (*)
+### Division (/)
+
+## Advance  Arithmetic Operation
+### Floor Division (//)
+### Modulus (%)
+### Exponentiation (**)
